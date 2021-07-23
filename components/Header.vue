75c1d15b7fa9c3a1e34efcea0a6e2f68c9c8f1e1
or1995 <omar199590@gmail.com>
2021-07-23 02:47:27 +0200

    first commit

---
+++ components/Header.vue
@@ -0,0 +1,135 @@
+<template>
+  <header>
+    <div class="logo">
+      <div>
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M13.775 17c1.164-3.451 6.225-6.519 6.225-17h-16c0 10.481 5.061 13.549 6.225 17h3.55zm-5.192-15c.012 4.066.877 8.291 3.058 11.952-2.723-3.055-4.786-6.732-5.023-11.952h1.965zm8.417 20.449v1.551h-10v-1.551c2.552 0 3.298-2.061 3.411-3.449h3.179c.112 1.389.785 3.449 3.41 3.449z"/></svg>
+      </div>
+      <h1>FBScores</h1>
+    </div>
+    <div class="search">
+      <button @click="() => {console.log('hi')}">
+        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M23.822 20.88l-6.353-6.354c.93-1.465 1.467-3.2 1.467-5.059.001-5.219-4.247-9.467-9.468-9.467s-9.468 4.248-9.468 9.468c0 5.221 4.247 9.469 9.468 9.469 1.768 0 3.421-.487 4.839-1.333l6.396 6.396 3.119-3.12zm-20.294-11.412c0-3.273 2.665-5.938 5.939-5.938 3.275 0 5.94 2.664 5.94 5.938 0 3.275-2.665 5.939-5.94 5.939-3.274 0-5.939-2.664-5.939-5.939z"/></svg>
+      </button>
+      <input type="text" placeholder="Search"/>
+    </div>
+    <ul class="links">
+      <li>
+        <nuxt-link class="link" to="/">Matches</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link class="link" to="/">Teams</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link class="link" to="/">Leagues</nuxt-link>
+      </li>
+      <li>
+        <nuxt-link class="link" to="/">Cups</nuxt-link>
+      </li>
+      <li></li>
+    </ul>
+  </header>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+  header {
+    display: flex;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 4rem;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 2rem;
+    background-color: var(--primary-color);
+  }
+
+  .logo {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .logo div svg {
+    fill: var(--secondary-color);
+    height: 2rem;
+    width: 2rem;
+  }
+
+  .logo div {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    margin-right: .2rem;
+    transform: rotate(-10deg);
+  }
+
+  .logo h1 {
+    color: var(--very-light-color);
+  }
+
+  .search {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .search input {
+    border: none;
+    background-color: var(--primary-light-color);
+    outline: none;
+    height: 2.5rem;
+    width: 20rem;
+    padding: 0 2.5rem;
+    border-radius: .2rem;
+    font-size: 1.2rem;
+    color: var(--very-light-color);
+  }
+
+  .search input::placeholder {
+    color: var(--light-color);
+  }
+
+  .search button {
+    z-index: 100000;
+    margin-right: -2rem;
+    background-color: transparent;
+    border: none;
+    outline: none;
+    width: 1.5rem;
+    height: 1.5rem;
+    cursor: pointer;
+  }
+
+  .search button svg {
+    fill: var(--light-color);
+    height: 1.3rem;
+    width: 1.3rem;
+  }
+
+  .links {
+    list-style: none;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 1rem;
+  }
+
+  .link {
+    font-size: 1.5rem;
+    text-decoration: none;
+    color: var(--very-light-color);
+  }
+
+  .link:hover {
+    color: var(--secondary-color);
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,40 @@
+<template>
+  <div>
+    <Header/>
+    <nuxt/>
+  </div>
+</template>
+
+<script>
+import Header from '../components/Header';
+export default {
+  components: {
+    Header
+  }
+}
+</script>
+
+<style>
+  @import url('https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&display=swap');
+  :root {
+    --primary-color: #DB2B39;
+    --primary-light-color: #da4a56;
+    --secondary-color: #F3A712;
+    --blue: #29335C;
+    --champ: #F0CEA0;
+    --very-light-color: #E1EFE6;
+    --light-color: #dfe0e0;
+    --dark-color: #000411;
+    --primary-font: 'Catamaran', sans-serif;
+  }
+
+  * {
+    padding: 0;
+    margin: 0;
+    box-sizing: border-box;
+    font-family: var(--primary-font);
+  }
+  body {
+  
+  }
+</style>
